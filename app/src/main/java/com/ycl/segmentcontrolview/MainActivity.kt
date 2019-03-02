8db060c78bcd12e6af8e1a32eec7cf92ed4cacfc
Y11621546 <11621546@qq.com>
2019-03-02 16:18:48 +0800

    增加垂直分段显示

--- app/src/main/java/com/ycl/segmentcontrolview/MainActivity.kt
+++ app/src/main/java/com/ycl/segmentcontrolview/MainActivity.kt
@@ -2,11 +2,28 @@ package com.ycl.segmentcontrolview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        segment.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.rb_qq -> {
+                    println("点击了QQ")
+                }
+                R.id.rb_wb -> {
+                    println("点击了微博")
+                }
+                R.id.rb_wx -> {
+                    println("点击了微信")
+                }
+                else -> {
+                }
+            }
+
+        }
     }
 }
--- segmentlib/src/main/java/com/ycl/segmentlib/SegmentControlView.kt
+++ segmentlib/src/main/java/com/ycl/segmentlib/SegmentControlView.kt
@@ -5,9 +5,12 @@ import android.graphics.Color
 import android.graphics.Color.RED
 import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.StateListDrawable
+import android.support.v7.widget.AppCompatRadioButton
 import android.util.AttributeSet
 import android.widget.LinearLayout
+import android.widget.RadioButton
 import android.widget.RadioGroup
+import java.lang.IllegalArgumentException
 
 class SegmentControlView : RadioGroup {
 
@@ -24,7 +27,7 @@ class SegmentControlView : RadioGroup {
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.SegmentControlView, 0, 0)
         segmentStrokeWidth =
-            typedArray.getInt(R.styleable.SegmentControlView_segment_strokeWidth, segmentStrokeWidth)
+            typedArray.getDimensionPixelSize(R.styleable.SegmentControlView_segment_strokeWidth, segmentStrokeWidth)
 
         segmentSelectedStrokeColor =
             typedArray.getColor(R.styleable.SegmentControlView_segment_selectedStrokeColor, segmentSelectedStrokeColor)
@@ -34,37 +37,48 @@ class SegmentControlView : RadioGroup {
                 segmentUnSelectedStrokeColor
             )
         segmentCornerRadius =
-            typedArray.getFloat(R.styleable.SegmentControlView_segment_cornerRadius, segmentCornerRadius)
+            typedArray.getDimension(R.styleable.SegmentControlView_segment_cornerRadius, segmentCornerRadius)
         init(context)
     }
 
     private fun init(context: Context) {
         //  if (childCount < 1) throw IllegalArgumentException("childCount < 1")
-
         mContext = context
-        this.orientation = RadioGroup.HORIZONTAL
         val groupGradientDrawable = GradientDrawable()
         groupGradientDrawable.cornerRadius = segmentCornerRadius
         groupGradientDrawable.setStroke(segmentStrokeWidth, segmentSelectedStrokeColor)
         this.setBackgroundDrawable(groupGradientDrawable)
+        this.showDividers = LinearLayout.SHOW_DIVIDER_MIDDLE
+        val dividerDrawable = GradientDrawable()
+        dividerDrawable.setColor(segmentSelectedStrokeColor)
+        this.dividerPadding = 0
         this.post {
-            this.showDividers = LinearLayout.SHOW_DIVIDER_MIDDLE
-            val dividerDrawable = GradientDrawable()
-            dividerDrawable.setColor(segmentSelectedStrokeColor)
-            println("控件高度：${this.measuredHeight}  真实高度：${dp2px(48f)}")
-            dividerDrawable.setSize(segmentStrokeWidth, this.measuredHeight)
-            this.dividerPadding = 0
+            if (orientation == RadioGroup.HORIZONTAL) {
+                dividerDrawable.setSize(segmentStrokeWidth, this.measuredHeight)
+            } else if (orientation == RadioGroup.VERTICAL) {
+                dividerDrawable.setSize(this.measuredHeight, segmentStrokeWidth)
+            }
             this.dividerDrawable = dividerDrawable
-
         }
+
+
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        println("SegmentControlView.onMeasure")
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        super.onLayout(changed, l, t, r, b)
+        println("SegmentControlView.onLayout")
     }
 
 
     override fun onFinishInflate() {
         super.onFinishInflate()
-        println("控件个数：$childCount")
+        if (childCount < 1) throw IllegalArgumentException("childCount cannot < 1")
         for (i in 0 until childCount) {
-
             val child = this.getChildAt(i)
             val stateListDrawable = StateListDrawable()
             val segmentSelectedDrawable = GradientDrawable()
@@ -72,79 +86,110 @@ class SegmentControlView : RadioGroup {
             val layoutParams = child.layoutParams
             when (i) {
                 0 -> {
-                    if (layoutParams is MarginLayoutParams) {
-                        layoutParams.bottomMargin = segmentStrokeWidth
-                        layoutParams.topMargin = segmentStrokeWidth
-                        layoutParams.leftMargin = segmentStrokeWidth
+                    if (child is RadioButton) {
+                        child.isChecked = true
                     }
-                    segmentSelectedDrawable.cornerRadii = floatArrayOf(
-                        segmentCornerRadius,
-                        segmentCornerRadius,
-                        0f,
-                        0f,
-                        0f,
-                        0f,
-                        segmentCornerRadius,
-                        segmentCornerRadius
-                    )
-                    segmentSelectedDrawable.setColor(Color.RED)
-                    segmentUnSelectedDrawable.cornerRadii = floatArrayOf(
-                        segmentCornerRadius,
-                        segmentCornerRadius,
-                        0f,
-                        0f,
-                        0f,
-                        0f,
-                        segmentCornerRadius,
-                        segmentCornerRadius
-                    )
-                    segmentUnSelectedDrawable.setColor(segmentUnSelectedStrokeColor)
-                    stateListDrawable.addState(intArrayOf(android.R.attr.state_checked), segmentSelectedDrawable)
-                    stateListDrawable.addState(intArrayOf(), segmentUnSelectedDrawable)
+                    if (orientation == RadioGroup.HORIZONTAL) {
+                        if (layoutParams is MarginLayoutParams) {
+                            layoutParams.bottomMargin = segmentStrokeWidth
+                            layoutParams.topMargin = segmentStrokeWidth
+                            layoutParams.leftMargin = segmentStrokeWidth
+                        }
+                        segmentSelectedDrawable.cornerRadii = floatArrayOf(
+                            segmentCornerRadius,
+                            segmentCornerRadius,
+                            0f,
+                            0f,
+                            0f,
+                            0f,
+                            segmentCornerRadius,
+                            segmentCornerRadius
+                        )
+                        segmentSelectedDrawable.setColor(Color.RED)
+                        segmentUnSelectedDrawable.cornerRadii = floatArrayOf(
+                            segmentCornerRadius,
+                            segmentCornerRadius,
+                            0f,
+                            0f,
+                            0f,
+                            0f,
+                            segmentCornerRadius,
+                            segmentCornerRadius
+                        )
+                        segmentUnSelectedDrawable.setColor(segmentUnSelectedStrokeColor)
+                    } else if (orientation == RadioGroup.VERTICAL) {
+                        if (layoutParams is MarginLayoutParams) {
+                            layoutParams.topMargin = segmentStrokeWidth
+                            layoutParams.leftMargin = segmentStrokeWidth
+                            layoutParams.rightMargin = segmentStrokeWidth
+                        }
+                        segmentSelectedDrawable.cornerRadii = floatArrayOf(
+                            segmentCornerRadius,
+                            segmentCornerRadius,
+                            segmentCornerRadius,
+                            segmentCornerRadius,
+                            0f,
+                            0f,
+                            0f,
+                            0f
+                        )
+                        segmentSelectedDrawable.setColor(Color.RED)
+                        segmentUnSelectedDrawable.cornerRadii = floatArrayOf(
+                            segmentCornerRadius,
+                            segmentCornerRadius,
+                            segmentCornerRadius,
+                            segmentCornerRadius,
+                            0f,
+                            0f,
+                            0f,
+                            0f
+                        )
+                        segmentUnSelectedDrawable.setColor(segmentUnSelectedStrokeColor)
+                    }
+
 
                 }
                 childCount - 1 -> {
                     if (layoutParams is MarginLayoutParams) {
                         layoutParams.bottomMargin = segmentStrokeWidth
-                        layoutParams.topMargin = segmentStrokeWidth
                         layoutParams.rightMargin = segmentStrokeWidth
+                        layoutParams.leftMargin = segmentStrokeWidth
                     }
                     segmentSelectedDrawable.cornerRadii = floatArrayOf(
                         0f,
                         0f,
-                        segmentCornerRadius,
-                        segmentCornerRadius,
-                        segmentCornerRadius,
-                        segmentCornerRadius,
                         0f,
-                        0f
+                        0f,
+                        segmentCornerRadius,
+                        segmentCornerRadius,
+                        segmentCornerRadius,
+                        segmentCornerRadius
                     )
                     segmentSelectedDrawable.setColor(RED)
                     segmentUnSelectedDrawable.cornerRadii = floatArrayOf(
                         0f,
                         0f,
-                        segmentCornerRadius,
-                        segmentCornerRadius,
-                        segmentCornerRadius,
-                        segmentCornerRadius,
                         0f,
-                        0f
+                        0f,
+                        segmentCornerRadius,
+                        segmentCornerRadius,
+                        segmentCornerRadius,
+                        segmentCornerRadius
                     )
                     segmentUnSelectedDrawable.setColor(segmentUnSelectedStrokeColor)
-                    stateListDrawable.addState(intArrayOf(android.R.attr.state_checked), segmentSelectedDrawable)
-                    stateListDrawable.addState(intArrayOf(), segmentUnSelectedDrawable)
                 }
                 else -> {
                     if (layoutParams is MarginLayoutParams) {
-                        layoutParams.bottomMargin = segmentStrokeWidth
-                        layoutParams.topMargin = segmentStrokeWidth
+                        layoutParams.leftMargin = segmentStrokeWidth
+                        layoutParams.rightMargin = segmentStrokeWidth
                     }
                     segmentSelectedDrawable.setColor(RED)
                     segmentUnSelectedDrawable.setColor(segmentUnSelectedStrokeColor)
-                    stateListDrawable.addState(intArrayOf(android.R.attr.state_checked), segmentSelectedDrawable)
-                    stateListDrawable.addState(intArrayOf(), segmentUnSelectedDrawable)
+
                 }
             }
+            stateListDrawable.addState(intArrayOf(android.R.attr.state_checked), segmentSelectedDrawable)
+            stateListDrawable.addState(intArrayOf(), segmentUnSelectedDrawable)
             child.setBackgroundDrawable(stateListDrawable)
         }
     }
