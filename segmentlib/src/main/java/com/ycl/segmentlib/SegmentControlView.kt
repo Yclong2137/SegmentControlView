fe34945877d380ba2fd00950137408291d4b6352
Y11621546 <11621546@qq.com>
2019-03-04 15:17:23 +0800

    控件优化

--- segmentlib/src/main/java/com/ycl/segmentlib/SegmentControlView.kt
+++ segmentlib/src/main/java/com/ycl/segmentlib/SegmentControlView.kt
@@ -1,10 +1,13 @@
 package com.ycl.segmentlib
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.Color.RED
 import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.StateListDrawable
+import android.support.v4.content.ContextCompat
+import android.support.v4.view.ViewCompat
 import android.util.AttributeSet
 import android.widget.LinearLayout
 import android.widget.RadioButton
@@ -16,10 +19,15 @@ import android.widget.RadioGroup
 class SegmentControlView : RadioGroup {
 
     private var mContext: Context? = null
+    //控件样式
     private var segmentStrokeWidth = dp2px(1f)
-    private var segmentUnSelectedStrokeColor = Color.WHITE
-    private var segmentSelectedStrokeColor = Color.BLUE
+    private var segmentStrokeColor = Color.BLUE
     private var segmentCornerRadius = dp2px(8f).toFloat()
+
+    //tab样式
+    private var tabSelectedColor = Color.BLUE
+    private var tabColor = Color.BLUE
+
 
     constructor(context: Context) : super(context) {
         init(context)
@@ -29,16 +37,15 @@ class SegmentControlView : RadioGroup {
         val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.SegmentControlView, 0, 0)
         segmentStrokeWidth =
             typedArray.getDimensionPixelSize(R.styleable.SegmentControlView_segment_strokeWidth, segmentStrokeWidth)
-
-        segmentSelectedStrokeColor =
-            typedArray.getColor(R.styleable.SegmentControlView_segment_selectedStrokeColor, segmentSelectedStrokeColor)
-        segmentUnSelectedStrokeColor =
-            typedArray.getColor(
-                R.styleable.SegmentControlView_segment_unSelectedStrokeColor,
-                segmentUnSelectedStrokeColor
-            )
+        segmentStrokeColor =
+            typedArray.getColor(R.styleable.SegmentControlView_segment_strokeColor, segmentStrokeColor)
         segmentCornerRadius =
             typedArray.getDimension(R.styleable.SegmentControlView_segment_cornerRadius, segmentCornerRadius)
+        tabSelectedColor =
+            typedArray.getColor(R.styleable.SegmentControlView_segment_tabSelectedColor, tabSelectedColor)
+        tabColor = typedArray.getColor(R.styleable.SegmentControlView_segment_tabColor, tabColor)
+
+
         init(context)
     }
 
@@ -47,11 +54,11 @@ class SegmentControlView : RadioGroup {
         mContext = context
         val groupGradientDrawable = GradientDrawable()
         groupGradientDrawable.cornerRadius = segmentCornerRadius
-        groupGradientDrawable.setStroke(segmentStrokeWidth, segmentSelectedStrokeColor)
-        this.setBackgroundDrawable(groupGradientDrawable)
+        groupGradientDrawable.setStroke(segmentStrokeWidth, segmentStrokeColor)
+        ViewCompat.setBackground(this,groupGradientDrawable)
         this.showDividers = LinearLayout.SHOW_DIVIDER_MIDDLE
         val dividerDrawable = GradientDrawable()
-        dividerDrawable.setColor(segmentSelectedStrokeColor)
+        dividerDrawable.setColor(segmentStrokeColor)
         this.dividerPadding = 0
         this.post {
             if (orientation == RadioGroup.HORIZONTAL) {
@@ -80,16 +87,14 @@ class SegmentControlView : RadioGroup {
         super.onFinishInflate()
         if (childCount < 1) throw IllegalArgumentException("childCount cannot < 1")
         for (i in 0 until childCount) {
-            val child = this.getChildAt(i)
+            val child = this.getChildAt(i) as? RadioButton ?: return
             val stateListDrawable = StateListDrawable()
             val segmentSelectedDrawable = GradientDrawable()
             val segmentUnSelectedDrawable = GradientDrawable()
             val layoutParams = child.layoutParams
             when (i) {
                 0 -> {
-                    if (child is RadioButton) {
-                        child.isChecked = true
-                    }
+                    child.isChecked = true
                     if (orientation == RadioGroup.HORIZONTAL) {
                         if (layoutParams is MarginLayoutParams) {
                             layoutParams.bottomMargin = segmentStrokeWidth
@@ -106,7 +111,7 @@ class SegmentControlView : RadioGroup {
                             segmentCornerRadius,
                             segmentCornerRadius
                         )
-                        segmentSelectedDrawable.setColor(Color.RED)
+                        segmentSelectedDrawable.setColor(tabSelectedColor)
                         segmentUnSelectedDrawable.cornerRadii = floatArrayOf(
                             segmentCornerRadius,
                             segmentCornerRadius,
@@ -117,7 +122,7 @@ class SegmentControlView : RadioGroup {
                             segmentCornerRadius,
                             segmentCornerRadius
                         )
-                        segmentUnSelectedDrawable.setColor(segmentUnSelectedStrokeColor)
+                        segmentUnSelectedDrawable.setColor(tabColor)
                     } else if (orientation == RadioGroup.VERTICAL) {
                         if (layoutParams is MarginLayoutParams) {
                             layoutParams.topMargin = segmentStrokeWidth
@@ -134,7 +139,7 @@ class SegmentControlView : RadioGroup {
                             0f,
                             0f
                         )
-                        segmentSelectedDrawable.setColor(Color.RED)
+                        segmentSelectedDrawable.setColor(tabSelectedColor)
                         segmentUnSelectedDrawable.cornerRadii = floatArrayOf(
                             segmentCornerRadius,
                             segmentCornerRadius,
@@ -145,7 +150,7 @@ class SegmentControlView : RadioGroup {
                             0f,
                             0f
                         )
-                        segmentUnSelectedDrawable.setColor(segmentUnSelectedStrokeColor)
+                        segmentUnSelectedDrawable.setColor(tabColor)
                     }
 
 
@@ -167,7 +172,7 @@ class SegmentControlView : RadioGroup {
                             0f,
                             0f
                         )
-                        segmentSelectedDrawable.setColor(RED)
+                        segmentSelectedDrawable.setColor(tabSelectedColor)
                         segmentUnSelectedDrawable.cornerRadii = floatArrayOf(
                             0f,
                             0f,
@@ -178,7 +183,7 @@ class SegmentControlView : RadioGroup {
                             0f,
                             0f
                         )
-                        segmentUnSelectedDrawable.setColor(segmentUnSelectedStrokeColor)
+                        segmentUnSelectedDrawable.setColor(tabColor)
                     } else if (orientation == RadioGroup.VERTICAL) {
                         if (layoutParams is MarginLayoutParams) {
                             layoutParams.bottomMargin = segmentStrokeWidth
@@ -195,7 +200,7 @@ class SegmentControlView : RadioGroup {
                             segmentCornerRadius,
                             segmentCornerRadius
                         )
-                        segmentSelectedDrawable.setColor(RED)
+                        segmentSelectedDrawable.setColor(tabSelectedColor)
                         segmentUnSelectedDrawable.cornerRadii = floatArrayOf(
                             0f,
                             0f,
@@ -206,7 +211,7 @@ class SegmentControlView : RadioGroup {
                             segmentCornerRadius,
                             segmentCornerRadius
                         )
-                        segmentUnSelectedDrawable.setColor(segmentUnSelectedStrokeColor)
+                        segmentUnSelectedDrawable.setColor(tabColor)
                     }
 
                 }
@@ -222,14 +227,14 @@ class SegmentControlView : RadioGroup {
                             layoutParams.rightMargin = segmentStrokeWidth
                         }
                     }
-                    segmentSelectedDrawable.setColor(RED)
-                    segmentUnSelectedDrawable.setColor(segmentUnSelectedStrokeColor)
+                    segmentSelectedDrawable.setColor(tabSelectedColor)
+                    segmentUnSelectedDrawable.setColor(tabColor)
 
                 }
             }
             stateListDrawable.addState(intArrayOf(android.R.attr.state_checked), segmentSelectedDrawable)
             stateListDrawable.addState(intArrayOf(), segmentUnSelectedDrawable)
-            child.setBackgroundDrawable(stateListDrawable)
+            ViewCompat.setBackground(child,stateListDrawable)
         }
     }
 
